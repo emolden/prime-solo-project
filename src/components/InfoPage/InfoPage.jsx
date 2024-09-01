5bf418fd22d6d6de109dcc4146cf3801a2fcbc5c
Emma Molden <emma.molden007@gmail.com>
2024-09-01 18:48:09 -0500

    dispatch for league data upon page load

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -6,6 +8,15 @@ import React from 'react';
 // or even care what the redux state is
 
 function InfoPage() {
+
+  const dispatch = useDispatch()
+
+  useEffect (() => {
+    console.log('info page loaded')
+      dispatch({ type: 'GET_LEAGUE_DATA'})
+  },);
+
+
   return (
     <div className="container">
       <p>Info Page</p>
