2d71b54722094b46eb9e6c27a0cbd091f9d00045
Emma Molden <emma.molden007@gmail.com>
2024-09-01 20:03:29 -0500

    league data successfully being saved in the leagueData reducer

--- src/redux/sagas/admin.saga.js
+++ src/redux/sagas/admin.saga.js
@@ -6,6 +6,8 @@ function* leagueData (action) {
     console.log('in leagueData saga function')
     try{
         const response = yield axios.get(`/api/admin`);
+        // console.log('response data from GET /api/admin in the leagueData saga is: ', response);
+        yield put({ type: 'SET_LEAGUE_DATA', payload: response.data });
     }catch(error) {
         console.log('leagueData had an error: ', error)
     }
