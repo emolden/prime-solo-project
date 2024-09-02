2161c5e7034f2581dd5981238c527bf281335f89
Emma Molden <emma.molden007@gmail.com>
2024-09-02 10:31:25 -0500

    mui grid is rendering on the dom

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -19,10 +21,58 @@ function InfoPage() {
       dispatch({ type: 'GET_LEAGUE_DATA'})
   },);
 
+  const columns = [
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    },
+  ];
+  
+  
+
 
   return (
     <div className="container">
       <p>Info Page</p>
+      <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        // rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
       <ul>
         {leagueData.map((person) => {
           return (
