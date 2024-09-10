444fd54c6a3bb1698f2fc0895d320cf34f48a354
Emma Molden <emma.molden007@gmail.com>
2024-09-10 10:34:34 -0500

    user position correctly rendering on the skill and experience page

--- src/redux/reducers/userPosition.reducer.js
+++ src/redux/reducers/userPosition.reducer.js
@@ -1,12 +1,12 @@
 // userPosition reducer is an array of objects containing the
 // position preferences for the user registering to play
 
-const userPositionReducer = (state = [], action) => {
+const userPositionReducer = (state = {}, action) => {
     console.log('in userPosition reducer:', action.payload)
 
     switch (action.type) {
         case 'SET_USER_POSITION':
-            return action.payload;
+            return action.payload[0];
         
         default:
             return state;
